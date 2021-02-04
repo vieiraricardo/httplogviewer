65befc1e84b917c38efa082600418d7dfb769906
vieiraricardo <hi@ricvieira.com>
2021-02-03 21:38:40 -0300

    remove linux dependecies

--- src/lib/screen.js
+++ src/lib/screen.js
@@ -1,5 +1,5 @@
+const fs = require('fs')
 const blessed = require('blessed')
-const { exec } = require('child_process')
 
 const screen = blessed.screen({
   smartCSR: true
@@ -46,7 +46,7 @@ const content = blessed.box({
   }
 })
 
-function renderScreen (args) {
+function renderScreen(args) {
   const { string: file, options } = args
 
   getIpList(file)
@@ -55,7 +55,7 @@ function renderScreen (args) {
 
   screen.on('resize', () => screen.render())
 
-  screen.key(['C-c', 'q'], function (ch, key) {
+  screen.key(['C-c', 'q'], function(ch, key) {
     screen.destroy()
   })
 
@@ -75,37 +75,52 @@ function renderScreen (args) {
   screen.render()
 }
 
-function getIpList (file) {
-  exec(`cut -d " " -f 1 ${file} | sort | uniq -c | sort -nr`, (err, data) => {
+function getIpList(file) {
+  fs.readFile(file, (err, data) => {
     if (err) return ipList.insertItem(0, err.stack)
-    data.split(/\n/).forEach((item, i) => {
-      ipList.insertItem(i, `{bold}${item}{/}`)
-    })
+    const log = data.toString()
+    const regexp = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?=\s)/g
+
+    let IPLIST = log.match(regexp).reduce((acc, cur) => {
+      if (!acc) return acc.set(cur, 1)
+
+      let ip = acc.get(cur)
+      return ip ? acc.set(cur, ip + 1) : acc.set(cur, 1)
+    }, new Map())
+
+    IPLIST[Symbol.iterator] = function*() {
+      yield* [...this.entries()].sort((a, b) => a[1] - b[1])
+    }
+
+    for (let [key, value] of IPLIST.entries()) {
+      ipList.insertItem(value, `  {green-fg}${value}{/green-fg}{|}${key}  `)
+    }
 
     ipList.focus()
     screen.render()
   })
 }
 
-function getLogByIp (ip, file, status) {
-  const command = status
-    ? `grep ${ip} ${file} | grep " ${status}" | cut -d " " -f 6- `
-    : `grep ${ip} ${file} | cut -d " " -f 6- `
+function getLogByIp(ip, file, status) {
+  fs.readFile(file, (err, data) => {
+    const logByIp = status
+      ? new RegExp(`(?<=${ip}.+)(".+"\\s${status}(?=\\s\\d))`, 'g')
+      : new RegExp(`(?<=${ip}.+)(".+"\\s\\d{3}(?=\\s\\d))`, 'g')
 
-  exec(command, { maxBuffer: 1024 * 1024 * 10 }, (err, data) => {
-    if (err) return content.setContent(err.stack)
+    let logArray = data.toString().match(logByIp)
 
-    content.setContent(data)
+    if (!logArray) {
+      content.setLine(15, `Log with status code ${status} not found.`)
+      screen.render()
+      return
+    }
+
+    const log = logArray.toString().replace(/,/g, '\n\n')
+
+    content.setContent(log)
     content.focus()
     screen.render()
   })
 }
 
 module.exports = renderScreen
-
-// fs.readFile(process.argv[2], (err, data) => {
-//   if (err) return console.log(err)
-//   const log = data.toString()
-//   const regexp = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/ig
-//   console.log(log.match(regexp))
-// })
